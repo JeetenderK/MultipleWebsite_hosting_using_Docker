a9868b91a411610ddd5694c85d5d81886546aae3
Jeetender Kewlani <59938462+JeetenderK@users.noreply.github.com>
2020-03-16 04:04:59 +0530

    Updated Bash script
    For checking if Docker is installed on instance or not

--- Bash_MulipleWordpress.bash
+++ Bash_MulipleWordpress.bash
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-docker-compose -f Docker-Compose.yml up -d
-
-printf "file_uploads = On\nmemory_limit = 64M\nupload_max_filesize = 64M\npost_max_size = 64M\nmax_execution_time = 600" > ~/uploads.ini
-
-docker-compose -f Docker-Compose-Wordpress.yml up -d
+if ["$(docker ps)"]
+then
+    docker-compose -f Docker-Compose.yml up -d
+		printf "file_uploads = On\nmemory_limit = 64M\nupload_max_filesize = 64M\npost_max_size = 64M\nmax_execution_time = 600" > ~/uploads.ini
+		docker-compose -f Docker-Compose-Wordpress.yml up -d
+else
+        echo Docker Not installed
+fi
