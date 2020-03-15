6cc9117e1856a621472bcaf36598aeff90f20353
Jeetender Kewlani <59938462+JeetenderK@users.noreply.github.com>
2020-03-16 04:29:10 +0530

    Update Bash_MulipleWordpress.bash

--- Bash_MulipleWordpress.bash
+++ Bash_MulipleWordpress.bash
@@ -2,9 +2,17 @@
 
 if ["$(docker ps)"]
 then
-    docker-compose -f Docker-Compose.yml up -d
+	if ["$(docker-compose --version)"]
+	then
+        docker-compose -f Docker-Compose.yml up -d
+		
+		# for setting maximum upload file size limit on php
 		printf "file_uploads = On\nmemory_limit = 64M\nupload_max_filesize = 64M\npost_max_size = 64M\nmax_execution_time = 600" > ~/uploads.ini
+		
 		docker-compose -f Docker-Compose-Wordpress.yml up -d
+	else
+		echo Docker Compose Not installed
+	fi
 else
         echo Docker Not installed
 fi
